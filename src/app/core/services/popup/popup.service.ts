e14e2e7e865c3902c15fa5951053bf23f97789f4
Emerson Franco <contato.emersonfranco@gmail.com>
2024-06-10 10:27:45 -0300

    feat: :sparkles: New method in pupup service ; ShowAsComponent

--- src/app/core/services/popup/popup.service.ts
+++ src/app/core/services/popup/popup.service.ts
@@ -1,9 +1,43 @@
-import { Injectable } from '@angular/core';
+import {
+  ApplicationRef,
+  EnvironmentInjector,
+  Injectable,
+  Type,
+  createComponent,
+} from '@angular/core';
+import { InterfaceGenericPopup } from '../../../shared/types/interfaces/InterfaceGenericPopup';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PopupService {
+  constructor(
+    private injector: EnvironmentInjector,
+    private applicationRef: ApplicationRef
+  ) {}
 
-  constructor() { }
+  public ShowAsComponent<T extends InterfaceGenericPopup>(
+    component: Type<T>,
+    conf: InterfaceGenericPopup
+  ): void {
+    const NewElement = document.createElement('simple-popup');
+
+    const ElementComponentRef = createComponent(component, {
+      environmentInjector: this.injector,
+      hostElement: NewElement,
+    });
+
+    this.applicationRef.attachView(ElementComponentRef.hostView);
+
+    ElementComponentRef.instance.closed.subscribe(() => {
+      this.applicationRef.detachView(ElementComponentRef.hostView);
+      ElementComponentRef.destroy();
+    })
+
+    ElementComponentRef.instance.title = conf.title;
+    ElementComponentRef.instance.message = conf.message;
+    ElementComponentRef.instance.theme = conf.theme;
+
+    document.body.appendChild(NewElement);
+  }
 }
