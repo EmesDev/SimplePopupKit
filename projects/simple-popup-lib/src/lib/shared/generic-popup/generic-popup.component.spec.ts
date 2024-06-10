6acced9667bc476113827be981ee05030ea311d2
Emerson Franco <contato.emersonfranco@gmail.com>
2024-06-10 13:56:52 -0300

    test: :test_tube: Generic popup component tests

--- projects/simple-popup-lib/src/lib/shared/generic-popup/generic-popup.component.spec.ts
+++ projects/simple-popup-lib/src/lib/shared/generic-popup/generic-popup.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { EnumPopupTheme } from 'simple-popup-lib';
 import { GenericPopupComponent } from './generic-popup.component';
 
 describe('GenericPopupComponent', () => {
@@ -20,4 +21,31 @@ describe('GenericPopupComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should bind title input', () => {
+    component.title = 'Test Title';
+    fixture.detectChanges();
+    expect(component.title).toBe('Test Title');
+  });
+
+  it('should bind message input', () => {
+    component.message = 'Test Message';
+    fixture.detectChanges();
+    expect(component.message).toBe('Test Message');
+  });
+
+  it('should bind theme input', () => {
+    component.theme = EnumPopupTheme.DEFAULT;
+    fixture.detectChanges();
+    expect(component.theme).toBe(EnumPopupTheme.DEFAULT);
+  });
+
+  it('should emit closed event when closed', () => {
+    spyOn(component.closed, 'emit');
+    component.closed.emit();
+    expect(component.closed.emit).toHaveBeenCalled();
+  });
+
+  
 });
