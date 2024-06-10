e76d7a2774f74264834457825980e8fd8b889c3a
Emerson Franco <contato.emersonfranco@gmail.com>
2024-06-10 10:29:20 -0300

    feat: :sparkles: interface generic popups

--- src/app/shared/types/interfaces/InterfaceGenericPopup.ts
+++ src/app/shared/types/interfaces/InterfaceGenericPopup.ts
@@ -3,5 +3,6 @@ import { EnumPopupTheme } from "../enums/EnumPopupTheme";
 export interface InterfaceGenericPopup {
     title: string;
     message: string;
+    closed?: any;
     theme: EnumPopupTheme
 }
