2721a6cfb4030a2e2f6f0fe4d3df5391f0cee0b9
Emerson Franco <contato.emersonfranco@gmail.com>
2024-06-10 09:56:55 -0300

    🚀 Setup: Init project

---
+++ src/app/shared/generic-popup/generic-popup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generic-popup',
+  standalone: true,
+  imports: [],
+  templateUrl: './generic-popup.component.html',
+  styleUrl: './generic-popup.component.css'
+})
+export class GenericPopupComponent {
+
+}
