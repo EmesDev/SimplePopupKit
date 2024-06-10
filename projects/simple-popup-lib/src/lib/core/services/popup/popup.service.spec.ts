c2225ee8ba339f0223a38a9cfd19e491d5577771
Emerson Franco <contato.emersonfranco@gmail.com>
2024-06-10 14:09:14 -0300

    test: :test_tube: Service spec

--- projects/simple-popup-lib/src/lib/core/services/popup/popup.service.spec.ts
+++ projects/simple-popup-lib/src/lib/core/services/popup/popup.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
+import { EnumPopupTheme, GenericPopupComponent } from 'simple-popup-lib';
 import { PopupService } from './popup.service';
 
 describe('PopupService', () => {
@@ -13,4 +14,31 @@ describe('PopupService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should show popup', () => {
+    service.ShowAsComponent(GenericPopupComponent, {
+      title: 'Test Title',
+      message: 'Test Message',
+      theme: EnumPopupTheme.DEFAULT,
+    });
+
+    expect(service).toBeTruthy();
+
+    
+  });
+
+  it('should show popup with timeout', () => {
+    service.ShowAsComponent(GenericPopupComponent, {
+      title: 'Test Title',
+      message: 'Test Message',
+      theme: EnumPopupTheme.DEFAULT,
+      timeout: 1000,
+    });
+
+    expect(service).toBeTruthy();
+  });
+
+
+
+
 });
