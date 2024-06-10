4a6037dfc83a2af79bae70d2440ad4bc9fd6d630
Emerson Franco <contato.emersonfranco@gmail.com>
2024-06-10 12:38:04 -0300

    feat: :sparkles: Generic Popup Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PopupService } from './core/services/popup/popup.service';
+import { GenericPopupComponent } from './shared/generic-popup/generic-popup.component';
+import { EnumPopupTheme } from './shared/types/enums/EnumPopupTheme';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  constructor(private popupService: PopupService) {}
   title = 'SimplePopupKit';
+
+  openPopup() {
+    this.popupService.ShowAsComponent(GenericPopupComponent, {
+      title: 'Popup Title',
+      message: 'Popup Message',
+      theme: EnumPopupTheme.DEFAULT,
+    });
+  }
 }
